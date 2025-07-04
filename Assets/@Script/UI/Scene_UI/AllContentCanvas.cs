443661694edcc771637a1b5e8d6b4c683b323bfd
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-04 20:23:03 +0900

    골드, 젬 증가 치트키 수정

--- Assets/@Script/UI/Scene_UI/AllContentCanvas.cs
+++ Assets/@Script/UI/Scene_UI/AllContentCanvas.cs
@@ -119,11 +119,11 @@ public class AllContentCanvas : UI_Scene
 
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            Manager.Game.SaveData.Gold += 10;
+            Manager.Game.SaveData.Gold += 100;
         }
         if (Input.GetKeyDown(KeyCode.F2))
         {
-            Manager.Game.SaveData.Gem += 10;
+            Manager.Game.SaveData.Gem += 100;
         }
     }
 }
