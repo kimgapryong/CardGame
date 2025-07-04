d8235a20618906400af1228c6a547604aa9ce816
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-05 00:25:47 +0900

    히어로 등급 색깔 적용

--- Assets/@Script/UI/Fragment/ShopHeroFragment.cs
+++ Assets/@Script/UI/Fragment/ShopHeroFragment.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.VisualScripting;
 using UnityEngine;
+using static Define;
 
 public class ShopHeroFragment : UI_Base
 {
@@ -16,6 +17,7 @@ public class ShopHeroFragment : UI_Base
     }
 
     HeroData _heroData;
+    Define.HeroRating _heroRating;
     int _priceGold;
 
     public override bool Init()
@@ -32,13 +34,30 @@ public class ShopHeroFragment : UI_Base
     public void SetInfo(HeroData heroData)
     {
         _heroData = heroData;
-        _priceGold = Manager.Data.HeroRatingPriceDatas[_heroData.Hero_Rating].gold;
+        _heroRating = _heroData.Hero_Rating;
+        _priceGold = Manager.Data.HeroRatingPriceDatas[_heroRating].gold;
 
         Manager.Resource.LoadAsync<Sprite>(_heroData.LevelData[0].Sprite, (sprite) =>
         {
             GetImage((int)Images.ProfileImage).sprite = sprite;
             GetText((int)Texts.NameText).text = _heroData.LevelData[0].HeroName;
             GetText((int)Texts.RatingText).text = _heroData.Hero_Rating.ToString();
+            switch (_heroRating)
+            {
+
+                case HeroRating.Common:
+                    GetText((int)Texts.RatingText).color = Color.gray;
+                    break;
+                case HeroRating.Normal:
+                    GetText((int)Texts.RatingText).color = Color.yellow;
+                    break;
+                case HeroRating.Epic:
+                    GetText((int)Texts.RatingText).color = new Color(160f / 255f, 32f / 255f, 240f / 255f);
+                    break;
+                case HeroRating.Legend:
+                    GetText((int)Texts.RatingText).color = Color.red;
+                    break;
+            }
             GetText((int)Texts.PriceText).text = _priceGold.ToString();
             GetText((int)Texts.PriceText).color = Manager.Game.SaveData.Gold >= _priceGold ? Color.white : Color.red;
         });
