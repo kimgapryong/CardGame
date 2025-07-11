92de946e4acd7eff7669338be364ab627bfe91dd
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-11 19:49:31 +0900

    a

---
+++ Assets/@Script/UI/Pop_UI/ChestPop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ChestPop : UI_Popup
+{
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+
+        return true;
+    }
+}
