3a9feb71fab6f7a6b67df53f1820e98fff5e1741
LSM-Shamrock <lsm.shamrock@gmail.com>
2025-07-11 22:31:43 +0900

    chest

--- Assets/@Script/UI/Pop_UI/ChestPop.cs
+++ Assets/@Script/UI/Pop_UI/ChestPop.cs
@@ -2,12 +2,24 @@
 
 public class ChestPop : UI_Popup
 {
+    public enum Images
+    {
+        ChestImage,
+    }
+
+    ChestData _chestData;
 
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
+        BindImage(typeof(Images));
+
+        Manager.Resource.LoadSprite(_chestData.OpenSprite, (sprite) =>
+        {
+            GetImage((int)Images.ChestImage).sprite = sprite;
+        });
 
         return true;
     }
